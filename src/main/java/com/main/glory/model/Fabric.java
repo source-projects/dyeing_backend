ea0aa8ed24f699f01e73678333517b2a0f9880cb
Salman-fullstack@github.com <Salman-fullstack@github.com>
2020-09-15 19:31:07 +0530

    fabricIn bux resolved

--- src/main/java/com/main/glory/model/Fabric.java
+++ src/main/java/com/main/glory/model/Fabric.java
@@ -8,14 +8,8 @@ import lombok.Setter;
 
 import java.util.Date;
 import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -47,7 +41,9 @@ public class Fabric {
 	private Long userHeadId;
 	private Long recordCount;
 
+
 	@OneToMany(cascade = CascadeType.ALL)
+	@Transient
 	@JoinColumn(name = "controlId", referencedColumnName = "id")
 	List<FabricInRecord> fabricInRecord;
 
