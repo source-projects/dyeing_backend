98b8cedbe7d57a4c089a85ab037b301859480b37
= <dhananirajr@gmail.com>
2020-09-17 11:53:25 +0530

    update shade

--- src/main/java/com/main/glory/model/shade/ShadeMast.java
+++ src/main/java/com/main/glory/model/shade/ShadeMast.java
@@ -1,10 +1,7 @@
 package com.main.glory.model.shade;
 
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -16,6 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 @Entity
 @Table(name = "ShadeMast")
+@ToString
 public class ShadeMast {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
