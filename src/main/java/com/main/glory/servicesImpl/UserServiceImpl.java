83e89dbdb8f030d7e86591db8edc83a011260f6e
MohanGloryAutotech <mohan.gloryautotech@gmail.com>
2020-11-10 16:22:46 +0530

    user Controller done

--- src/main/java/com/main/glory/servicesImpl/UserServiceImpl.java
+++ src/main/java/com/main/glory/servicesImpl/UserServiceImpl.java
@@ -4,10 +4,14 @@ import com.main.glory.Dao.user.UserDao;
 import com.main.glory.model.user.UserData;
 import com.main.glory.services.UserServiceInterface;
 
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service("userServiceImpl")
 public class UserServiceImpl implements UserServiceInterface {
@@ -42,4 +46,35 @@ public class UserServiceImpl implements UserServiceInterface {
         return userDao.findByUserNameAndPassword(userName, password);
     }
 
+    public List<UserData> getAllHeadUser() {
+        List<UserData> adminList = userDao.findByUserHeadId(0l);
+        List<UserData> userHeads = new ArrayList<>();
+        adminList.forEach(e -> {
+            List<UserData> users = userDao.findByUserHeadId(e.getId());
+            userHeads.addAll(users);
+        });
+        return userHeads;
+    }
+
+    public int isAvailable(UserData userData) {
+
+        var userData1 = userDao.findById(userData.getId());
+        if(!userData1.isPresent())
+        {
+            return 0;
+        }
+        userDao.save(userData);
+        return 1;
+
+    }
+
+    public boolean deletePartyById(Long id) {
+        var userIndex= userDao.findById(id);
+        if(!userIndex.isPresent())
+            return false;
+        else
+            userDao.deleteById(id);
+        return true;
+
+    }
 }
