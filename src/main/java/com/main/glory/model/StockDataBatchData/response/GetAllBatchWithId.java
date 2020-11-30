a8d987045296a9cad693a69e1abaefc15b40d50d
MohanGloryAutotech <mohan.gloryautotech@gmail.com>
2020-11-30 13:19:30 +0530

    get batch by quality and party

---
+++ src/main/java/com/main/glory/model/StockDataBatchData/response/GetAllBatchWithId.java
@@ -0,0 +1,15 @@
+package com.main.glory.model.StockDataBatchData.response;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class GetAllBatchWithId {
+    Long id;
+    String batchId;
+
+}
