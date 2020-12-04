422c432a363e159636c747a50934c4748952420a
MohanGloryAutotech <mohan.gloryautotech@gmail.com>
2020-12-04 14:23:21 +0530

    Unit added in response

--- src/main/java/com/main/glory/model/quality/response/GetAllQualtiy.java
+++ src/main/java/com/main/glory/model/quality/response/GetAllQualtiy.java
@@ -15,11 +15,14 @@ public class GetAllQualtiy {
     private String  qualityId;
     private String  qualityName;
     private String qualityType;
+    private String unit;
+
 
     public GetAllQualtiy(Quality quality) {
         this.id=quality.getId();
         this.qualityId=quality.getQualityId();
         this.qualityName=quality.getQualityName();
         this.qualityType=quality.getQualityType();
+        this.unit=quality.getUnit();
     }
 }
