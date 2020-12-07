2e8740ac234e9541509313f9d6d8e85d4258a7b4
Mohan Bhombe <72798492+MohanGloryAutotech@users.noreply.github.com>
2020-12-07 12:10:03 +0530

    Default values for the attribute

--- src/main/java/com/main/glory/model/StockDataBatchData/BatchData.java
+++ src/main/java/com/main/glory/model/StockDataBatchData/BatchData.java
@@ -28,22 +28,5 @@ public class BatchData {
     Long controlId;
     Boolean isProductionPlanned=false;
     Boolean isExtra=false;
-    Long sequenceId;
-    Double finishMtr;
-    Boolean isBillGenrated=false;
-
-    public BatchData(BatchData other) {
-        this.id=other.id;
-        this.mtr=other.mtr;
-        this.wt=other.wt;
-        this.batchId=other.batchId;
-        this.controlId=other.controlId;
-        this.isExtra=other.isExtra;
-        this.sequenceId=other.sequenceId;
-        this.finishMtr=other.finishMtr;
-        this.isBillGenrated=other.isBillGenrated;
-
-    }
-
-
-}
+    Long sequenceId=0l;
+    Double finishMtr=0.0;Dea
