7003cb7292044f4c7fca7ad6e6e3ccc38c935e69
MohanGloryAutotech <mohan.gloryautotech@gmail.com>
2020-12-17 18:33:21 +0530

    model created for pdf export

--- src/main/java/com/main/glory/servicesImpl/PartyServiceImp.java
+++ src/main/java/com/main/glory/servicesImpl/PartyServiceImp.java
@@ -1,9 +1,18 @@
 package com.main.glory.servicesImpl;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.main.glory.model.SendEmail;
+import com.main.glory.model.document.request.GetDocumentModel;
 import com.main.glory.model.party.request.AddParty;
 import com.main.glory.model.party.request.PartyWithName;
 import com.main.glory.model.party.request.PartyWithPartyCode;
@@ -176,4 +185,56 @@ public class PartyServiceImp implements PartyServiceInterface{
 
 		return false;
     }
+
+    //send pdf for mail
+	public void sendPdfForParty(GetDocumentModel documentModel) throws Exception {
+		String fileName = "party.pdf";
+		File f=new File(fileName);
+		f.createNewFile();
+		Document document = new Document();
+		PdfWriter.getInstance(document, new FileOutputStream(fileName));//file is created, where the project filder is
+		document.open();
+
+
+		//Add the data
+		PdfPTable table = new PdfPTable(5);
+		PdfPCell partyName = new PdfPCell(new Phrase("Party Name"));
+		table.addCell(partyName);
+		PdfPCell partyAddress = new PdfPCell(new Phrase("Address"));
+		table.addCell(partyAddress);
+		PdfPCell partyContact = new PdfPCell(new Phrase("Contact no"));
+		table.addCell(partyContact);
+		PdfPCell partyCity = new PdfPCell(new Phrase("City"));
+		table.addCell(partyCity);
+		PdfPCell partyState = new PdfPCell(new Phrase("State"));
+		table.addCell(partyState);
+		table.setHeaderRows(1);
+
+		List<Party> party = partyDao.findAll();
+
+		if(documentModel.getToRow()>party.size())
+			throw new Exception("Party size is :"+party.size());
+
+		for (int i =documentModel.getFromRow().intValue()-1 ; i < (documentModel.getToRow()); i++) {
+
+			//System.out.println(party.get(i).getPartyName());
+			table.addCell(party.get(i).getPartyName());
+			table.addCell(party.get(i).getPartyAddress1());
+			table.addCell(party.get(i).getContactNo());
+			table.addCell(party.get(i).getCity());
+			table.addCell(party.get(i).getCity());
+
+		}
+
+		document.add(table);
+
+		document.close();
+
+		//______Document created successfully
+
+
+		//Send mail
+		SendEmail email=new SendEmail(documentModel.getToEmail(), fileName,documentModel.getSubjectEmail(),documentModel.getSendText());
+		email.sendMail();
+	}
 }
