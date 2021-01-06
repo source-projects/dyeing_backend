923ce547ed2ce36d260137ffb23fb36a25dfd300
MohanGloryAutotech <mohan.gloryautotech@gmail.com>
2021-01-06 17:16:22 +0530

    invoice save api's fix

--- src/main/java/com/main/glory/Dao/dispatch/DispatchMastDao.java
+++ src/main/java/com/main/glory/Dao/dispatch/DispatchMastDao.java
@@ -4,7 +4,12 @@ import com.main.glory.model.dispatch.DispatchMast;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface DispatchMastDao extends JpaRepository<DispatchMast,Long> {
     @Query("select MAX(dm.postfix) from DispatchMast dm where prefix=:invoiceType")
     Long getInvoiceNumber(String invoiceType);
+
+    @Query("select d from DispatchMast d")
+    List<DispatchMast> getAllInvoiceList();
 }
