832609c4c5301cf9ee3e829976d5d85432519a01
MohanGloryAutotech <mohan.gloryautotech@gmail.com>
2021-01-30 17:23:03 +0530

    Process add/remove api added

--- src/main/java/com/main/glory/controller/DyeingProcessController.java
+++ src/main/java/com/main/glory/controller/DyeingProcessController.java
@@ -35,6 +35,21 @@ public class DyeingProcessController extends ControllerConfig{
         }
     }
 
+    @PutMapping("/dyeingProcess/update")
+    public GeneralResponse<Boolean> updateDyeingProcess(@RequestBody DyeingProcessMast data){
+        try {
+            if(data == null){
+                return new GeneralResponse<>(false, "No data passed, please send valid data", false, System.currentTimeMillis(), HttpStatus.BAD_REQUEST);
+            }else {
+                dyeingProcessService.updateDyeingProcess(data);
+                return new GeneralResponse<>(true, "Data updated Successfully", true, System.currentTimeMillis(), HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new GeneralResponse<>(false, e.getMessage(), false, System.currentTimeMillis(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     @GetMapping("/dyeingProcess/all")
     public GeneralResponse<List<GetAllDyeingProcessList>> getAllDyeingProcess(){
         try{
@@ -64,6 +79,26 @@ public class DyeingProcessController extends ControllerConfig{
             return new GeneralResponse<>(null, e.getMessage(), false, System.currentTimeMillis(), HttpStatus.NOT_FOUND);
         }
         return new GeneralResponse<>(null, "no data found", false, System.currentTimeMillis(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @DeleteMapping("/dyeingProcess/deleteProcessById/{id}")
+    public GeneralResponse<Boolean> deleteProcessById(@PathVariable(name = "id") Long id){
+        try{
+            if(id==null)
+                throw new Exception("id can't be null");
+
+            Boolean flag = dyeingProcessService.deleteByProcessId(id);
+            if(flag){
+                return new GeneralResponse<>(true, "deleted successfully", true, System.currentTimeMillis(), HttpStatus.FOUND);
+            }
+            else
+                return new GeneralResponse<>(null, "no data found", false, System.currentTimeMillis(), HttpStatus.NOT_FOUND);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return new GeneralResponse<>(null, e.getMessage(), false, System.currentTimeMillis(), HttpStatus.BAD_REQUEST);
+        }
+
     }
 
 
