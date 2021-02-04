aa9e8878e163c78cd1ef1f8549fee9eb9b71103e
MohanGloryAutotech <mohan.gloryautotech@gmail.com>
2021-02-04 17:30:29 +0530

    change - 2

--- src/main/java/com/main/glory/servicesImpl/JetServiceImpl.java
+++ src/main/java/com/main/glory/servicesImpl/JetServiceImpl.java
@@ -103,7 +103,7 @@ public class JetServiceImpl {
         ProductionPlan productionPlanExits=productionPlanService.getProductionData(productionId);
         if(productionPlanExits.getStatus())
             throw new Exception("production is already added into jet");
-/*
+
         Optional<ShadeMast> shadeMast = shadeService.getShadeMastById(productionPlanExits.getShadeId());
 
         if(shadeMast.isEmpty())
@@ -114,7 +114,7 @@ public class JetServiceImpl {
         //count the color amt to deduct
         Double colorAmtToDeduct=0.0;
         Double totalBatchWt=stockBatchService.getWtByControlAndBatchId(productionPlanExits.getStockId(),productionPlanExits.getBatchId());
-
+/*
         //check the capacity first for the color box issue had that much capcity to fill the batch or not
         for(ShadeData shadeData:shadeMast.get().getShadeDataList())
         {
@@ -245,7 +245,7 @@ public class JetServiceImpl {
                 }
 
 
-            }
+            }*/
 
             // 2. now also enter the entire data of process into the slip table if the above condition is fulfilled as per the requirement
             DyeingProcessMast dyeingProcessMast = dyeingProcessService.getDyeingProcessById(shadeMast.get().getProcessId());
@@ -290,7 +290,7 @@ public class JetServiceImpl {
             dyeingSlipMast.setDyeingSlipDataList(dyeingSlipDataList);
             dyeingSlipService.saveDyeingSlipMast(dyeingSlipMast);
 
-*/
+
             // 3. change the status of production
             productionPlanExist.setStatus(true);
             productionPlanService.saveProductionPlan(productionPlanExist);
