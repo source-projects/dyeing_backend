71495583172bc5e4076593b0f6f2c999829f96d5
MohanGloryAutotech <mohan.gloryautotech@gmail.com>
2021-02-05 15:06:31 +0530

    change s

--- src/main/java/com/main/glory/servicesImpl/StockBatchServiceImpl.java
+++ src/main/java/com/main/glory/servicesImpl/StockBatchServiceImpl.java
@@ -1003,6 +1003,7 @@ public class StockBatchServiceImpl {
 
         for(GetBatchWithControlId batch : data)
         {
+            //System.out.println("batchwt:"+batch.getWT());
             StockMast stockMast = stockMastDao.findByStockId(batch.getControlId());
             if(stockMast==null)
                 continue;
@@ -1011,7 +1012,7 @@ public class StockBatchServiceImpl {
             if (quality==null)
                 continue;
 
-            Party party = partyDao.findByPartyId(stockMast.getQualityId());
+            Party party = partyDao.findByPartyId(stockMast.getPartyId());
             if(party==null)
                 continue;
 
