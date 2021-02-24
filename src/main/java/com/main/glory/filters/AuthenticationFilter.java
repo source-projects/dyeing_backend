5a8af50853bf5f2cad9c003af2867842fca4bad7
Harshit kyal <harshitkyal@gmail.com>
2021-02-25 00:19:00 +0530

    remove JWT check

--- src/main/java/com/main/glory/filters/AuthenticationFilter.java
+++ src/main/java/com/main/glory/filters/AuthenticationFilter.java
@@ -158,9 +158,9 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 				}
 			}
 
-			if(id == null){
-				throw new Exception("No JWT found");
-			}
+// 			if(id == null){
+// 				throw new Exception("No JWT found");
+// 			}
 
 			chain.doFilter(request, response);
 		} catch(Exception e){
