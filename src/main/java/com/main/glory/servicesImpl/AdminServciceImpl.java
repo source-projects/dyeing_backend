691cd97b2bad895e1a4ce3920878ca23f4d2e2b1
MohanGloryAutotech <mohan.gloryautotech@gmail.com>
2021-03-01 13:07:39 +0530

    adde the machine record api

--- src/main/java/com/main/glory/servicesImpl/AdminServciceImpl.java
+++ src/main/java/com/main/glory/servicesImpl/AdminServciceImpl.java
@@ -91,9 +91,6 @@ public class AdminServciceImpl {
             approveByDao.deleteApprovedById(id);
             return true;
 
-
-
-
     }
 
     public void saveDepartment(Department c) throws Exception {
