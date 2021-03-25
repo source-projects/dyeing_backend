74959d5dc0fe9dddabca994e8b04671e57b15e4b
Mohan Bhombe <72798492+MohanGloryAutotech@users.noreply.github.com>
2021-03-25 14:43:57 +0530

    Update AttendanceServiceImpl.java

--- src/main/java/com/main/glory/servicesImpl/AttendanceServiceImpl.java
+++ src/main/java/com/main/glory/servicesImpl/AttendanceServiceImpl.java
@@ -63,7 +63,7 @@ public class AttendanceServiceImpl {
         Attendance attendance = attendanceDao.getLatestAttendanceRecordByEmployeeId(id);
 
 
-        if(attendance.getInTime()!=null && attendance.getOutTime()!=null || attendance!=null)//attendance.getInTime()!=null && attendance.getOutTime()!=null)
+        if(attendance.getInTime()!=null && attendance.getOutTime()!=null || attendance==null)//attendance.getInTime()!=null && attendance.getOutTime()!=null)
         {
             employeeWithAttendance =new EmployeeWithAttendance(employeeMastExist,new Attendance());
         }
