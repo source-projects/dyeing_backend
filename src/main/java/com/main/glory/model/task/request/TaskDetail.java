728505a3eef94f75666e2c7f0aeab3432308f80a
MohanGloryAutotech <mohan.gloryautotech@gmail.com>
2021-04-05 15:23:30 +0530

    chang in response

--- src/main/java/com/main/glory/model/task/request/TaskDetail.java
+++ src/main/java/com/main/glory/model/task/request/TaskDetail.java
@@ -1,10 +1,15 @@
 package com.main.glory.model.task.request;
 
 import com.main.glory.model.task.TaskData;
+import com.main.glory.model.task.TaskDataImage;
+import com.main.glory.model.task.TaskMast;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,4 +20,20 @@ public class TaskDetail extends TaskData {
     String detail;
     String taskPriority;
     String taskType;
+
+
+
+    public TaskDetail(TaskData taskData, String detail, String taskPriority, String taskType) {
+        super(taskData);
+        this.detail = detail;
+        this.taskPriority = taskPriority;
+        this.taskType = taskType;
+    }
+
+    public TaskDetail(TaskData taskData,TaskMast taskMast) {
+        super(taskData);
+        this.detail = taskMast.getDetail();
+        this.taskPriority = taskMast.getTaskPriority();
+        this.taskType = taskMast.getTaskType();
+    }
 }
