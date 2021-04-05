22c60f16cf2bd95ba83f13344514238caaae9d74
Mohan Bhombe <72798492+MohanGloryAutotech@users.noreply.github.com>
2021-04-05 12:02:44 +0530

    Update MergeBatchController.java

--- src/main/java/com/main/glory/controller/MergeBatchController.java
+++ src/main/java/com/main/glory/controller/MergeBatchController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@RestController
+@RequestMapping("/api")
 public class MergeBatchController extends ControllerConfig {
 
     @Autowired
