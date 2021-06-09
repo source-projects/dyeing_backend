ad5aee4584c0bc671bd5c299d306e5674e06a878
MohanGloryAutotech <mohan.gloryautotech@gmail.com>
2021-06-09 21:09:56 +0530

    change in floating value

--- src/main/java/com/main/glory/servicesImpl/DyeingSlipServiceImpl.java
+++ src/main/java/com/main/glory/servicesImpl/DyeingSlipServiceImpl.java
@@ -196,7 +196,7 @@ public class DyeingSlipServiceImpl {
             }
 
 
-
+            //System.out.println("batch:"+productionPlan.getBatchId());
             GetAllProductionWithShadeData record = productionPlanService.getProductionWithColorToneByBatchId(productionPlan.getBatchId());
 
             slipFormatData.setTotalWt(stockBatchService.changeInFormattedDecimal(wt));
