e2c7f9e70797c57a25451371542d969e0387d940
MohanGloryAutotech <mohan.gloryautotech@gmail.com>
2021-06-25 14:30:19 +0530

    change in batch return mast

--- src/main/java/com/main/glory/model/StockDataBatchData/response/BatchReturnResponse.java
+++ src/main/java/com/main/glory/model/StockDataBatchData/response/BatchReturnResponse.java
@@ -28,11 +28,13 @@ public class BatchReturnResponse {
     String gst;
     String broker;
     String tempoNo;
+    String pChallanNo;
     List<BatchReturnData> batchReturnList;
 
     public BatchReturnResponse(BatchReturnMast batchReturnExist, Long pcs, List<BatchReturnData> batchReturnList, Double totalMtr) {
         this.id = batchReturnExist.getId();
         this.chlNo = batchReturnExist.getChlNo();
+        this.pChallanNo = batchReturnExist.getPChallanNo()==null?null:batchReturnExist.getPChallanNo();
         this.totalMtr=totalMtr;
         this.totalPcs = pcs;
         this.partyName=batchReturnExist.getPartyName();
