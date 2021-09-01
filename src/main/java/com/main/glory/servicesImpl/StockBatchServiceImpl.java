e4fd76fde1d30c159af18a7875bfd7bcdd92b2ab
kartikquagnitia <kartik@quagnitia.co.in>
2021-09-01 12:31:02 +0530

    minor issue fix

--- src/main/java/com/main/glory/servicesImpl/StockBatchServiceImpl.java
+++ src/main/java/com/main/glory/servicesImpl/StockBatchServiceImpl.java
@@ -416,7 +416,7 @@ public class StockBatchServiceImpl {
 
         }
 
-        FilterResponse<GetAllStockWithPartyNameResponse> response=new FilterResponse<GetAllStockWithPartyNameResponse>(list,queryResponse.getNumber()+1,queryResponse.getSize(),queryResponse.getTotalPages());
+        FilterResponse<GetAllStockWithPartyNameResponse> response=new FilterResponse<GetAllStockWithPartyNameResponse>(list,queryResponse.getNumber(),queryResponse.getSize(),queryResponse.getTotalPages());
 
         return response;
     }
