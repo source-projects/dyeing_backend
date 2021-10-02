bd8a49e97c8933c31f9db514cfa019cf73ebcf41
MohanGloryAutotech <mohan.gloryautotech@gmail.com>
2021-10-02 11:39:48 +0530

    change in supplier dao

--- src/main/java/com/main/glory/model/supplier/responce/ItemWithSupplier.java
+++ src/main/java/com/main/glory/model/supplier/responce/ItemWithSupplier.java
@@ -1,11 +1,13 @@
 package com.main.glory.model.supplier.responce;
 
+
+import com.main.glory.model.supplier.Supplier;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
+
 @NoArgsConstructor
 @Getter
 @Setter
@@ -14,4 +16,21 @@ public class ItemWithSupplier {
     String itemName;
     Long supplierId;
     Double rate;
+    Supplier supplier;
+
+
+    public ItemWithSupplier(Long id, String itemName, Long supplierId, Double rate) {
+        this.id = id;
+        this.itemName = itemName;
+        this.supplierId = supplierId;
+        this.rate = rate;
+    }
+
+    public ItemWithSupplier(Long id, String itemName, Long supplierId, Double rate, Supplier supplier) {
+        this.id = id;
+        this.itemName = itemName;
+        this.supplierId = supplierId;
+        this.rate = rate;
+        this.supplier = supplier;
+    }
 }
